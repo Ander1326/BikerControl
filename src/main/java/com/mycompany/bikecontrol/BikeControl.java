93c2ada083639eedccba6b85f5ed3c6507bd05f8
TUF <TUF@10.157.12.111>
2025-04-28 15:00:45 -0500

    BikerControl

---
+++ src/main/java/com/mycompany/bikecontrol/BikeControl.java
@@ -0,0 +1,15 @@
+package com.mycompany.bikecontrol;
+/**
+ *
+ * @author TUF
+ */
+import com.mycompany.bikecontrol.IGU.Principal;
+public class BikeControl {
+
+    public static void main(String[] args) {
+       Principal pric = new Principal();
+       pric.setVisible(true);
+       pric.setLocationRelativeTo(null);
+       
+    }
+}
---
+++ src/main/java/com/mycompany/bikecontrol/Logica/Administrador.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bikecontrol.Logica;
+
+/**
+ *
+ * @author TUF
+ */
+public class Administrador {
+     private int id_administrador;
+     private String nombre;
+     private String contrasena;
+     
+     
+}
